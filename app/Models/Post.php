5418e9799c0fe24a50d4aeecde778ee9f37e77dc
Leon0726a <leon.yoshikoshi0726@gmail.com>
2023-09-04 17:08:00 +0900

    feat: ブログ投稿一覧のページネーション追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,9 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    public function getByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
         // updated_atを降順に並べたあと、limitで件数制限をかける
-	return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+	return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
