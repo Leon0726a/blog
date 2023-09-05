b3bc636bcc6f6b55ee34ae5ae7aa77edf32a0f20
Leon0726a <leon.yoshikoshi0726@gmail.com>
2023-09-05 13:02:28 +0900

    feat:ブログ投稿作成画面作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,19 +10,20 @@
 
     </head>
     <body class="antialiased">
-        <h1>Blog Name</h1>
-        <div class='posts'>
-	  @foreach ($posts as $post)
-          <div class='post'>
-            <h2 class='title'>
-	      <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-	    </h2>
-            <p class='body'>{{ $post->body }}</p>
-          </div>
-	  @endforeach
+      <h1>Blog Name</h1>
+      <a href='/posts/create'>create</a>
+      <div class='posts'>
+	@foreach ($posts as $post)
+        <div class='post'>
+          <h2 class='title'>
+	    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+	  </h2>
+          <p class='body'>{{ $post->body }}</p>
         </div>
-	<div class='paginate'>
-	  {{ $posts->links() }}
-	</div>
+	@endforeach
+      </div>
+      <div class='paginate'>
+	{{ $posts->links() }}
+      </div>
     </body>
 </html>
