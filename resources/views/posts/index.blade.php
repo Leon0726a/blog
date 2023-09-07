00e726c33ac5d9c2812e9131690c6068a28a020b
Leon0726a <leon.yoshikoshi0726@gmail.com>
2023-09-07 16:11:29 +0900

    feat:ブログ投稿作成画面の保存時入力チェック処理実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>blogl</title>
+        <title>blog</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
