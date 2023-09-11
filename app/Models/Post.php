2d6cf6c30e3ac702079698c1c0f164133cce8537
Leon0726a <leon.yoshikoshi0726@gmail.com>
2023-09-11 13:39:29 +0900

    feat:ブログ投稿の編集機能

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,7 @@ class Post extends Model
         // updated_atを降順に並べたあと、limitで件数制限をかける
 	return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
     protected $fillable = [
         'title',
 	'body',
